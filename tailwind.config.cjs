c5d7847e1c5fb14f4f98d05b84f9a23b86c70e82
Rob Pfleghardt <Rob@sagecg.com>
2025-10-18 14:47:53 -0400

    UI/Sidebar fixes + Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // optional but recommended if you use `prose` classes:
+    // require('@tailwindcss/typography'),
+  ],
+}
