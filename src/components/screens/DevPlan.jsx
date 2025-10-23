913a50ec1bf474d11681543e9cbc626581774a3b
Rob Pfleghardt <Rob@sagecg.com>
2025-10-23 06:31:46 -0400

    Rebuild

--- src/components/screens/DevPlan.jsx
+++ src/components/screens/DevPlan.jsx
@@ -1008,9 +1008,6 @@ Thank you for your candid input!
         </div>
     );
 };
-// FIX: Added Icon for Copy to Clipboard in the RequestFeedbackModal (needed because it's used above)
-//const Copy = ({ className }) => <svg xmlns="http://www.w3.org/2000/svg" className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" 
-//strokeLinejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/></svg>;
 
 
 // --- Component 2: Tracker Dashboard View ---
