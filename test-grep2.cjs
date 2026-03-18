42d9c55b825c36483cd107eab83b89ca2dddb4d4
rob637 <rob@sagecg.com>
2026-03-18 15:47:48 +0000

    chore: clean up 90+ temp scripts from root directory
    Deleted one-time debug/fix/patch scripts that were created during development.
    Useful diagnostic scripts already exist in scripts/debug/

--- test-grep2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/components/widgets/ThisWeeksActionsWidget.jsx', 'utf8');
-const lines = content.split('\n');
-lines.forEach((l, i) => {
-  if (l.includes('.attended === true')) {
-    console.log(i + 1, l);
-  }
-});
